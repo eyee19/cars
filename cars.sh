ab25726fe7ed57f0f8b57965d48a22b03e002d4e
Everett Yee <eyee305@gmail.com>
2018-11-28 11:45:17 -0800

    Added adding cars function, quit function

--- cars.sh
+++ cars.sh
@@ -2,4 +2,32 @@
 # cars.sh
 # Everett Yee
 
+sep=":"
+
+while true; do
+
+echo "Welcome to the old cars inventory. Choose an option by typing 1, 2, or 3:
+----------------------------
+Option 1: Add a car	
+Option 2: View list of cars
+Option 3: Quit
+----------------------------"
+read INPUT
+
+	case "$INPUT" in
+		"1") echo "Enter car year (e.g. 2003):"
+		read YEAR
+		echo "Enter car make (e.g. Toyota):"
+		read MAKE
+		echo "Enter car model (e.g. Camry):"
+		read MODEL
+		concatString="$YEAR$sep$MAKE$sep$MODEL"
+		echo $concatString >> my_old_cars.txt
+		echo "Added to inventory";;
+		"2") echo "Here's the list of current cars in the inventory:"
+		break;;
+		"3") echo "Goodbye!"
+		 break;;
+	esac
+done
 
